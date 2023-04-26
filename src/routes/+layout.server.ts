12a80c04d1adf50b70b57e5e7de7c05c2e595209
Ercode Techoglu <ercode@pm.me>
2023-04-26 18:54:40 +0300

    giriş yapan kullanıcıları bilgilerim sayfasına gönder

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,23 @@
 // src/routes/+layout.server.ts
 
-export const load = async ({ locals: { getSession } }) => {
+import { redirect } from '@sveltejs/kit';
+
+export const load = async ({ route, locals: { supabase, getSession } }) => {
+	const session = await getSession();
+
+	const profile = await supabase.from('profiles').select().eq('user_id', session?.user.id).single();
+
+	if (session && !profile.data && route.id !== '/(protected)/bilgilerim') {
+		throw redirect(302, '/bilgilerim');
+	}
+
+	// Bilgilerim sayfasına kullanıcı tekrar ulaşamaz.
+	// Bilgileri düzenleme özelliği eklenince bunu kaldır.
+	if (session && profile.data && route.id === '/(protected)/bilgilerim') {
+		throw redirect(302, '/');
+	}
+
 	return {
-		session: await getSession()
+		session
 	};
 };
